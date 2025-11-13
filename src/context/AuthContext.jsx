a5ce3a6cc60d37ad056638ed8c88129c7151aaaf
leduy0203 <lecongduy0203@gmail.com>
2025-11-13 21:20:41 +0700

    fix

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -48,6 +48,7 @@ export const AuthProvider = ({ children }) => {
   const logout = () => {
     localStorage.removeItem("user");
     localStorage.removeItem("token");
+    sessionStorage.removeItem("lowStockNotificationShown");
     setUser(null);
   };
 
